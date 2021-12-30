533576662e527d32f1f9e8a7322fcbf4ce0641a1
Sekarre <zimny2121@gmail.com>
2021-12-30 17:37:10 +0100

    Fixing cors and angular settings

---
+++ gateway/src/main/java/iw/sb/springdockergateway/CorsGlobalConfiguration.java
@@ -0,0 +1,17 @@
+package iw.sb.springdockergateway;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsGlobalConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+          .allowedOrigins("*")
+          .allowedMethods("GET")
+          .maxAge(3600);
+    }
+}
